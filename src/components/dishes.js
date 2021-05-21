dc45f739d647a4c3ffca64959aa2c48fdfbce994
RakeshMehto17 <rakeshmehto17@gmail.com>
2021-05-21 15:11:35 +0530

    final2

--- src/components/dishes.js
+++ src/components/dishes.js
@@ -1,72 +1,86 @@
-export const DISHES=[
-    {
-      id: 0,
-      name:'Banana',
-      image: 'assets/images/banana.png',
-      label:'Hot',
-      description:'What goodness is in a banana? Other than being rich in vitamin B6, bananas are a good source of vitamin C, dietary fibre and manganese.'},
+export const DISHES = [
+  {
+    id: 0,
+    name: "Banana",
+    image: "assets/images/banana.png",
+    label: "Hot",
+    description:
+      "What goodness is in a banana? Other than being rich in vitamin B6, bananas are a good source of vitamin C, dietary fibre and manganese.",
+  },
 
-   {
-      id: 1,
-      name:'Eggs',
-      image: 'assets/images/egg.png',
-      label:'New',
-      description:'This comes with 77 calories, 6 grams of protein and 5 grams of healthy fats.Eggs also contain various trace nutrients that are important for health.In fact, eggs are pretty much the perfect food. They contain a little bit of almost every nutrient you need.If you can get your hands on pastured or omega-3 enriched eggs, these are even better. '                        },
+  {
+    id: 1,
+    name: "Eggs",
+    image: "assets/images/egg.png",
+    label: "New",
+    description:
+      "This comes with 77 calories, 6 grams of protein and 5 grams of healthy fats.Eggs also contain various trace nutrients that are important for health.In fact, eggs are pretty much the perfect food. They contain a little bit of almost every nutrient you need.If you can get your hands on pastured or omega-3 enriched eggs, these are even better. ",
+  },
 
-         {
-            id: 2,
-            name:'keto butter chicken',
-            image: 'assets/images/keto-butter-chicken.png',
-            label:'',
-            description:' This keto friendly butter chicken recipe is just the right thing if you want something healthy yet delicious. Here creamy gravy is subtly laced with chicken chunks and desi spices.'                        },
+  {
+    id: 2,
+    name: "keto butter chicken",
+    image: "assets/images/keto-butter-chicken.png",
+    label: "",
+    description:
+      " This keto friendly butter chicken recipe is just the right thing if you want something healthy yet delicious. Here creamy gravy is subtly laced with chicken chunks and desi spices.",
+  },
 
-   {
-      id: 3,
-      name:'Turmeric Masala milk',
-      image: 'assets/images/turmeric-milk.png',
-      label:'',
-      description:"Turmeric milk is something our mothers and grandmothers are familiar with, and it’s often referred to as liquid gold! With the healing and anti-inflammatory properties of turmeric, combined with the benefits of other spices, this is the perfect drink to develop immunity and stay healthy"},
+  {
+    id: 3,
+    name: "Turmeric Masala milk",
+    image: "assets/images/turmeric-milk.png",
+    label: "",
+    description:
+      "Turmeric milk is something our mothers and grandmothers are familiar with, and it’s often referred to as liquid gold! With the healing and anti-inflammatory properties of turmeric, combined with the benefits of other spices, this is the perfect drink to develop immunity and stay healthy",
+  },
 
-      {
-         id: 4,
-      name:'Oats Mango Smoothie',
-      image: 'assets/images/smoothie.png',
-      label:'New',
-      description:'Smoothies are a popular pick during the summer season. they are wholesome, delicious and refreshing. And so are mangoes! Here is a stellar smoothie recipe with the goodness of mangoes and wholesome oats.'
-      },
-      {
-      id: 5,
-      name:'Superfood Salad',
-      image: 'assets/images/salad.png',
-      label:'New',
-      description:'For those looking to strengthen their immune system, this gluten-free and vegan salad is an immunity quick-fix. It is packed with delicious superfoods like chickpeas, kale, and blueberries that are rich in nutrients like vitamin A, vitamin C, calcium, iron, and antioxidants.'
-      },
-      {
-         id: 6,
-         name:'Ginger And Mulethi Wali Chai ',
-         image: 'assets/images/Ginger-tea.png',
-      label:'New',
-      description:'A perfect immunity booster, this ginger-mulethi tea is brimming with health benefits. A comforting tea that can come handy when dealing with cold and cough.'
-      },
-      {
-         id: 7,
-         name:'Cream of Almond Soup',
-         image: 'assets/images/soup.png',
-      label:'New',
-      description:'A creamy, rich soup with a nutty flavour of almonds. Packed with the goodness of protein, this almond soup has simple ingredients and can be made in less than an hour.'
-      },
-      {
-         id: 8,
-         name:'Bajra Dosa',
-         image: 'assets/images/dosa.png',
-      label:'New',
-      description:'Millet dosa is a healthy and gluten free south indian beakfast recipe. You Can make this dosa under 20 mintunes, It is good option for the diabetics.'
-      },
-      {
-         id: 9,
-         name:'Chicken And Corn Soup',
-         image: 'assets/images/chicken-soup.png',
-      label:'New',
-      description:'A silky texture corn soup with chicken, eggs and jalapenos. This comfort bowl makes for a full lunch or dinner and is packed with nutrients.'
-      }
-   ]
+  {
+    id: 4,
+    name: "Oats Mango Smoothie",
+    image: "assets/images/smoothie.png",
+    label: "New",
+    description:
+      "Smoothies are a popular pick during the summer season. they are wholesome, delicious and refreshing. And so are mangoes! Here is a stellar smoothie recipe with the goodness of mangoes and wholesome oats.",
+  },
+  {
+    id: 5,
+    name: "Superfood Salad",
+    image: "assets/images/salad.png",
+    label: "New",
+    description:
+      "For those looking to strengthen their immune system, this gluten-free and vegan salad is an immunity quick-fix. It is packed with delicious superfoods like chickpeas, kale, and blueberries that are rich in nutrients like vitamin A, vitamin C, calcium, iron, and antioxidants.",
+  },
+  {
+    id: 6,
+    name: "Ginger And Mulethi Wali Chai ",
+    image: "assets/images/Ginger-tea.png",
+    label: "New",
+    description:
+      "A perfect immunity booster, this ginger-mulethi tea is brimming with health benefits. A comforting tea that can come handy when dealing with cold and cough.",
+  },
+  {
+    id: 7,
+    name: "Cream of Almond Soup",
+    image: "assets/images/soup.png",
+    label: "New",
+    description:
+      "A creamy, rich soup with a nutty flavour of almonds. Packed with the goodness of protein, this almond soup has simple ingredients and can be made in less than an hour.",
+  },
+  {
+    id: 8,
+    name: "Bajra Dosa",
+    image: "assets/images/dosa.png",
+    label: "New",
+    description:
+      "Millet dosa is a healthy and gluten free south indian beakfast recipe. You Can make this dosa under 20 mintunes, It is good option for the diabetics.",
+  },
+  {
+    id: 9,
+    name: "Chicken And Corn Soup",
+    image: "assets/images/chicken-soup.png",
+    label: "New",
+    description:
+      "A silky texture corn soup with chicken, eggs and jalapenos. This comfort bowl makes for a full lunch or dinner and is packed with nutrients.",
+  },
+];
